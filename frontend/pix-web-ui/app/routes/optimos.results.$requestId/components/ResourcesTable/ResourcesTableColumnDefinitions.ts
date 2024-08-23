e1eead71c0ce052b7d05dc516861cc12c2145845
Jannis Rosenbaum <j.ro@dock51.de>
2024-08-23 09:13:21 +0300

    fix worktime display

--- frontend/pix-web-ui/app/routes/optimos.results.$requestId/components/ResourcesTable/ResourcesTableColumnDefinitions.ts
+++ frontend/pix-web-ui/app/routes/optimos.results.$requestId/components/ResourcesTable/ResourcesTableColumnDefinitions.ts
@@ -17,10 +17,10 @@ export const COLUMN_DEFINITIONS: {
   minWidth?: string | number;
 }[] = [
   { id: "name", label: "Name", formatFn: (x) => x, minWidth: "10em" },
-  { id: "total_worktime", label: "Worktime", formatFn: formatMilliseconds, lowerIsBetter: false, minWidth: "10em" },
+  { id: "total_worktime", label: "Worktime", formatFn: formatSeconds, lowerIsBetter: false, minWidth: "10em" },
   { id: "available_time", label: "Available Time", formatFn: formatSeconds, lowerIsBetter: true },
   { id: "cost_per_hour", label: "Hourly Rate", formatFn: formatHourlyRate, lowerIsBetter: true },
-  { id: "total_cost", label: "Total Cost", formatFn: formatCurrency, lowerIsBetter: true },
+  { id: "total_cost", label: "Cost/week", formatFn: formatCurrency, lowerIsBetter: true },
   { id: "utilization", label: "Utilization", formatFn: formatPercentage, lowerIsBetter: false },
   { id: "is_human", label: "Type", formatFn: (x) => (x ? "Human" : "Machine") },
   { id: "max_weekly_cap", label: "Max h/week", formatFn: formatHours, lowerIsBetter: false },
